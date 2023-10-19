b3c76ff7b36739a97518d7095caed2a8dbd71dad
Levente <leventemajor36@gmail.com>
2023-10-19 10:26:37 +0200

    dsa

--- Program.cs
+++ Program.cs
@@ -13,7 +13,8 @@ using (var sr = new StreamReader(@"..\..\..\src\helsinki.txt", Encoding.UTF8))
     }
 }
 
-Console.WriteLine($"3.Feladat: {sportagak.Count()}");
+Console.WriteLine($"3.Feladat:");
+Console.WriteLine($"Pontszerző helyezések száma: {sportagak.Count()}");
 
 int arany = sportagak.Count(x => x.Helyezes == 1);
 int ezust = sportagak.Count(x => x.Helyezes == 2);
@@ -24,3 +25,11 @@ Console.WriteLine($"\tArany: {arany}");
 Console.WriteLine($"\tEzust: {ezust}");
 Console.WriteLine($"\tBronz: {bronz}");
 Console.WriteLine($"\tÖsszesen: {arany+ezust+bronz}");
+
+int negyedik = sportagak.Count(x => x.Helyezes == 4);
+int otodik = sportagak.Count(x => x.Helyezes == 5);
+int hatodik = sportagak.Count(x => x.Helyezes == 6);
+
+int olimpiaiPontok = arany*7 + ezust*5 + bronz*4 + negyedik*3 + otodik*2 + hatodik;
+Console.WriteLine("5.Feladat:");
+Console.WriteLine($"\tOlimpiai pontok száma: {olimpiaiPontok}");
