20edf46a67bea0ba74b9ac9b2d8defc6e5bb1f73
Levente <leventemajor36@gmail.com>
2023-10-19 10:22:56 +0200

    task 2

--- Program.cs
+++ Program.cs
@@ -14,3 +14,13 @@ using (var sr = new StreamReader(@"..\..\..\src\helsinki.txt", Encoding.UTF8))
 }
 
 Console.WriteLine($"3.Feladat: {sportagak.Count()}");
+
+int arany = sportagak.Count(x => x.Helyezes == 1);
+int ezust = sportagak.Count(x => x.Helyezes == 2);
+int bronz = sportagak.Count(x => x.Helyezes == 3);
+
+Console.WriteLine($"4.Feladat:");
+Console.WriteLine($"\tArany: {arany}");
+Console.WriteLine($"\tEzust: {ezust}");
+Console.WriteLine($"\tBronz: {bronz}");
+Console.WriteLine($"\tÖsszesen: {arany+ezust+bronz}");
