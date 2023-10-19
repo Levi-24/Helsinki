3a6791b5e063a346d1155232f58b360bfc527589
Levente <leventemajor36@gmail.com>
2023-10-19 10:17:10 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Helsinki;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+List<Sportag> sportagak = new List<Sportag>();
+using (var sr = new StreamReader(@"..\..\..\src\helsinki.txt", Encoding.UTF8))
+{
+    while (!sr.EndOfStream)
+    {
+        sportagak.Add(new Sportag(sr.ReadLine()));
+    }
+}
+
+Console.WriteLine($"3.Feladat: {sportagak.Count()}");
---
+++ Sportag.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helsinki
+{
+    internal class Sportag
+    {
+        public int Helyezes { get; set; }
+        public int SportolokSzama { get; set; }
+        public string SportagNeve { get; set; }
+        public string VersenyszamNeve { get; set; }
+
+        public Sportag(string sor)
+        {
+            var darab = sor.Split(' ');
+            this.Helyezes = int.Parse(darab[0]);
+            this.SportolokSzama = int.Parse(darab[1]);
+            this.SportagNeve = darab[2];
+            this.VersenyszamNeve = darab[3];
+        }
+    }
+}
